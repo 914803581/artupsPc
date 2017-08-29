71329981eb1a333b5f44dee65c19c3eba116beb1
LoadChange <soocto@gmail.com>
2017-08-29 19:09:06 +0800

    construct

--- build/check-versions.js
+++ build/check-versions.js
@@ -46,3 +46,22 @@ module.exports = function () {
     process.exit(1)
   }
 }
+
+console.log('                                                                                                    ');
+console.log('        ..                                                                   ,. .                   ');
+console.log('     ;WWWW.                                                          .ffjjf; KE.KL     ,GGGGGGGGGGG:');
+console.log('    .KWWWWf               :;                                         :WKKWWj;Wt DW     ;WKEEEEEEEKW,');
+console.log('    iWWDWWK.             tWD                                           . Kf.EWKEKWEE.  ;WG:::::::DW,');
+console.log('    DWWiDWW;    .;;, ,i ,KWE;,. :;,: ;;  .;;;;;,.   ,,;;;;,,.         DE KfiWKLGWKLL   ,WKEEEEEEKKW,');
+console.log('   iWWW:fWWE    iWWWDWW jWWWWWi LWWDGWW ,KWWWWWWWj  EWWWWWWWWt        Kf KjWWE  KD     ,WG.......LW,');
+console.log('   LWWD ;WWW,   iWWWKDG.jWWKDE, fKWWKDD LWWKDDKWWK  EWWEDDKWWE       :WLtKGEEKttWEti   ;WWWWWWWWWWW,');
+console.log('   KWK;  EWWf   iWWW.   jWWD    fWWK.   DWWL  iWWK  EWWj  iWWK       :KKWWWiE#KEWWKD             .. ');
+console.log('  tWWK   tWWW   iWWW.   jWWD    fWWE    DWWL  iWWW  EWWj  iWWE        ::KWf EE .KD.    LGGGGGGGGGGGL');
+console.log('  DWWEtttjKWWi  iWWW.   jWWD    fWWE    DWWL  iWWW  EWWj  tWWE         jWKL EKiiWEi;   KKLffffffffKK');
+console.log(' :WWWWWWWWWWWG  iWWW    jWWE    fWWE    DWWL  iWWK  EWWj  tWWE        iW,Ef EWWWWWWK.  EE:::::::::EK');
+console.log(' LWWWKKKKKKWWK: iWWW    jWWD    fWWE    DWWL  iWWK  EWWj  tWWE       ;WD Kf EE  KE     EWKKKKKKKKWWE');
+console.log('.KWWD.    ;WWWL iWWW    tWWE.   fWWE    DWWL  tWWW  EWWj  tWWE       iE. Kf EE  KD     EE.........EK');
+console.log('tWWWj     .EWWK iWWW.   ,WWWKKi fWWE    fWWWKKWKWK  EWWj  tWWE        ,GDWf EWKKWWKK;  EKEEEEEEEEKWK');
+console.log('GKKK:      LKKK.iKKK.    fKKKK; fKKD     GKKKKKKD,  DKKj  tKKE        .KKD: EKiiii;i.  EKffffffjffKK');
+console.log('                                                                                                    ');
+
