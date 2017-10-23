9a7b43e5c07b09b17366bf19c7d021d3398a1c1e
LoadChange <soocto@gmail.com>
2017-10-23 16:07:26 +0800

    磁铁画

--- src/page/citiehua/citiehua.js
+++ src/page/citiehua/citiehua.js
@@ -21,65 +21,68 @@ new Vue({
     category: '',
     picturePrefix: '//test.artup.com/img/',
     shufflingList: ['index_03.jpg', 'index_04.jpg', 'index_05.jpg', 'index_06.jpg', 'index_07.jpg'],
-    sizeList: [{
-      id: 'A',
-      size: '300x400',
-      price: 399,
-      hxprice: 79
-    }, {
-      id: 'B',
-      size: '400x300',
-      price: 399,
-      hxprice: 79
-    }, {
-      id: 'C',
-      size: '400x600',
-      price: 499,
-      hxprice: 119
-    }, {
-      id: 'D',
-      size: '600x400',
-      price: 499,
-      hxprice: 119
-    }],
-    borderList: [{
-      color: 'yms',
-      name: '原木色实木框'
-    }, {
-      color: 'hs',
-      name: '黑色实木框'
-    }],
+    drawData: null,
+    drawCoreData: null,
+    priceMap: {},
     currentSelect: {
       type: 0,
       size: 0,
-      border: 0
+      border: 0,
+      price: 0
     }
   },
   methods: {
-    /*更新sku*/
-    // updataSkuData() {
-    //   this.skuCode = this.getFromSession("category") + '.' + this.nowSize + '.' + this.nowType
-    //   var jsons = {
-    //     category: this.getFromSession("category"),
-    //     parameter: this.skuCode
-    //   }
-    //   Api.sku.querySku(jsons).then(res => {
-    //     console.log(res)
-    //     this.previewImageUrl = res.data.previewImageUrl //框形预览图
-    //     this.editImageUrl = res.data.editImageUrl //编辑框背景图
-    //     this.price = res.data.price
-    //     this.templateCode = res.data.templateCode
-    //     this.skuId = res.data.skuId
-    //   })
-    // },
     select: function (type, size, border) {
-      size = !size ? this.sizeList[0] : size
-      border = !border ? this.borderList[0] : border
-      this.currentSelect = {
-        type: type,
-        size: size,
-        border: border
+      let newVal = {type: type, price: ''}
+      if (size) {
+        newVal.size = size
       }
+      if (border) {
+        newVal.border = border
+      }
+      if (type === 1) {
+        if (!newVal.size && !this.currentSelect.size) {
+          newVal.size = this.drawData.attributes[1].attributeValues[0].code
+        }
+        if (!newVal.border && !this.currentSelect.border) {
+          newVal.border = this.drawData.attributes[0].attributeValues[0]
+        }
+      }
+      this.currentSelect = Object.assign(this.currentSelect, newVal)
+      let category = type === 1 ? 'citiehua' : 'citiehuaxin'
+      let parameter = `${category}.${this.currentSelect.size}.${this.currentSelect.border.code}`
+      if (!this.priceMap[parameter]) {
+        this._querySku(category, parameter)
+      } else {
+        this.currentSelect.price = this.priceMap[parameter]
+      }
+    },
+    drawCoreFill: function (res) {
+      this.drawCoreData = res.data
+    },
+    drawFill: function (res) {
+      this.drawData = res.data
+    },
+    /**
+     * 获取 磁铁画、磁铁画芯 套餐
+     * @private
+     */
+    _queryAttributes: function (category, fun) {
+      Api.sku.queryAttributes({
+        category: category
+      }).then(res => {
+        fun(res)
+      })
+    },
+    _querySku: function (category, parameter) {
+      let _self = this
+      Api.sku.querySku({
+        category: category,
+        parameter: parameter
+      }).then(res => {
+        _self.currentSelect.price = res.data.price
+        _self.priceMap[parameter] = res.data.price
+      })
     }
   },
   components: {
@@ -88,28 +91,10 @@ new Vue({
   },
   created: function () {
     document.body.style.cssText = 'opacity:1;'
-  },
-  mounted() {
-    let vm = this
-    //获取url的category值 以字符串的json格式保存到sessionStroage中
-    let queryObj = {'category': this.getQueryString('category')}
-    vm.category = this.getQueryString('category')
-    sessionStorage.setItem("urlQuery", vm.category)
-    //获取磁贴画的类型
-    Api.sku.queryAttributes(queryObj).then(res => {
-      if (res) {
-        console.log('磁贴画', res)
-      }
-    }, err => {
-      console.log(err)
-    })
-    //获取磁贴画芯的类型
-    Api.sku.queryAttributes({'category': 'citiehuaxin'}).then(res => {
-      if (res) {
-        console.log('磁贴画心', res)
-      }
-    }, err => {
-      console.log(err)
-    })
+    this.category = this.getQueryString('category')
+    sessionStorage.setItem('urlQuery', this.category);
+    [this.category, 'citiehuaxin'].forEach(function (item) {
+      this._queryAttributes(item, item === 'citiehuaxin' ? this.drawCoreFill : this.drawFill)
+    }.bind(this))
   }
 })
